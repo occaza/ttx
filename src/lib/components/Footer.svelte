3f6d07f0e01c7a9c3e37194b9711bd9f97e7ea92
occaza <alenaproject.it@gmail.com>
2025-09-26 20:04:39 +0700

    forgot to add link

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,9 @@
 <footer class="footer-center footer footer-horizontal rounded bg-base-200 p-10 text-base-content">
 	<nav class="grid grid-flow-col gap-4">
-		<a class="link link-hover">About us</a>
-		<a class="link link-hover">Contact</a>
-		<a class="link link-hover">Jobs</a>
-		<a class="link link-hover">Press kit</a>
+		<a href="/" class="link link-hover">Contact</a>
+		<a href="/" class="link link-hover">About us</a>
+		<a href="/" class="link link-hover">Jobs</a>
+		<a href="/" class="link link-hover">Press kit</a>
 	</nav>
 	<nav>
 		<div class="grid grid-flow-col gap-4">
